e171136c9ddfaa36dae144093d4641dc91a0e51f
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-11-27 12:18:10 +0200

    working example with images

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -12,6 +12,13 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization(allow => [allow.owner()]),
+  Song: a
+    .model({
+      id: a.id().required(),
+      name: a.string().required(),
+      coverArtPath: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,7 +26,10 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'userPool',
+    defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: {
+      expiresInDays: 365,
+    }
   },
 });
 
@@ -28,7 +38,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
---
+++ src/components/Todos.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import type { Schema } from "../../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+
+const client = generateClient<Schema>();
+
+function Todos() {
+  const { signOut, user } = useAuthenticator();
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id }, {
+      authMode: 'userPool'
+    
+    })
+  }
+
+  useEffect(() => {
+    const sub = client.models.Todo.observeQuery().subscribe({
+      next: ({ items }) => {
+        setTodos([...items]);
+      },
+    });
+    return () => sub.unsubscribe();
+  }, []);
+
+  function createTodo() {
+    client.models.Todo.create({ content: window.prompt("Todo content") }, {
+      authMode: 'userPool'
+    });
+  }
+
+  return (
+    <main>
+      <h1>{user?.signInDetails?.loginId} todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (          
+          <li 
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>
+            {todo.content}
+            </li>
+        ))}
+      </ul>
+      <button onClick={signOut}>Sign out</button>
+    </main>
+  );
+}
+
+export default Todos;
