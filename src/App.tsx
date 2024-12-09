3ba4240cf934b6efde0417b60054885ba11c0e4c
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-12-09 13:28:04 +0200

    reorder game components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import GameWrapper from "./components/GameWrapper";
+
 
 function App() {
 
 
   return (
     <main>
-      <GameWrapper></GameWrapper>
+
     </main>
   );
 }
--- src/components/x0/GameIntro.tsx
+++ src/components/x0/GameIntro.tsx
@@ -1,5 +1,5 @@
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import type { Schema } from "../../amplify/data/resource";
+import type { Schema } from "../../../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 type GameIntroProps = {
